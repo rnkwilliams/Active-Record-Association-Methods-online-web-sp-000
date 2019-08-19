98746c006dc9e6072e34b579dedea86a6790e48e
Ranika Williams <rnkprice07@gmail.com>
2019-08-19 18:22:35 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -14,6 +14,7 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
-    artists = self.artist.collect do ||
+    artists = self.artist.collect do |artist|
+      artist.name
   end
 end
